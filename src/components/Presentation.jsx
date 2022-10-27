5a4cebaf13e0037847a8f65ed0805e229c528ca9
MaxTuron <maxx@turon.fr>
2022-10-27 15:03:04 +0200

    Gestion d'un mauvais ID.

--- src/components/Presentation.jsx
+++ src/components/Presentation.jsx
@@ -1,27 +1,8 @@
-import React, {useEffect, useState} from "react";
-import {useParams} from 'react-router-dom';
-import  {getName}  from "../utils/fetchData"
+import React from "react";
 import "../styles/presentation.css"
 
-export default function Presentation() {
-
-  const { userId } = useParams();
-
-  const [name, setName] = useState(null);
-
-  async function afficheData () {
-    try{
-      const userResponse = await getName(userId);
-      setName(userResponse.name);
-
-    } catch(err) {
-      console.log(err)
-    }
-  }
-
-  useEffect(() => {
-   afficheData()
-  },[])
+export default function Presentation(props) {
+  const {name} = props;
 
     return (
       <div className="presentation">
--- src/pages/Accueil.jsx
+++ src/pages/Accueil.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import Header from "../components/Header"
 import Sidebar from "../components/Sidebar";
 import Presentation from "../components/Presentation"
@@ -7,38 +7,63 @@ import BarChart from "../components/BarChart"
 import LineCharts from "../components/LineCharts"
 import RadialBarCharts from "../components/RadialBarCharts"
 import RadarCharts from "../components/RadarCharts"
+import  {getName}  from "../utils/fetchData"
+import {useParams} from 'react-router-dom';
 import "../styles/accueil.css"
 
 export default function Accueil() {
 
+  const { userId } = useParams();
 
-  return (
-    <div>
-      <header>
-        <Header />
-      </header>
-      
-      <div className="allPage">
+  const [name, setName] = useState(null);
 
-        <aside className="leftBar"><Sidebar/></aside>
+  async function afficheData () {
+    try{
+      const userResponse = await getName(userId);
+      setName(userResponse.name);
+    } catch(err) {
+      setName(null);
+      console.log(err)
+    }
+  }
 
-        <div className="mainContent">
-            <div className="intro">
-              <Presentation />
-            </div>
-            <div className="barCharts">
-              <BarChart />
-            </div>
-            <div className="otherCharts">
-              <LineCharts />
-              <RadarCharts />
-              <RadialBarCharts />
-            </div>
+  useEffect(() => {
+   afficheData()
+  },[])
+
+  if(name == null){
+    return(
+      <p>Null</p>
+    )
+  }else {
+    return (
+      <div>
+        <header>
+          <Header />
+        </header>
+        
+        <div className="allPage">
+  
+          <aside className="leftBar"><Sidebar/></aside>
+  
+          <div className="mainContent">
+              <div className="intro">
+                <Presentation name={name}/>
+              </div>
+              <div className="barCharts">
+                <BarChart />
+              </div>
+              <div className="otherCharts">
+                <LineCharts />
+                <RadarCharts />
+                <RadialBarCharts />
+              </div>
+          </div>
+  
+          <aside className="rightBar"><Depenses /></aside>
         </div>
-
-        <aside className="rightBar"><Depenses /></aside>
-      </div>
-
-    </div> 
-  );
+  
+      </div> 
+    );
+  }
 }
